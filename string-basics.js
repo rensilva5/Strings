8844fc026dad621660a20c142eddb4791486a352
Renzo Silva <renzoasilva@gmail.com>
2022-07-14 11:51:13 -0400

    Trying examples on how to create new repos

---
+++ string-basics.js
@@ -0,0 +1,15 @@
+const message = "Hello Javascript";
+//message.toLocaleLowerCase()
+
+const msgToLowerCase = message.toLowerCase()
+const msgToLowerCase = message.toUpperCase()
+//const msgToLowerCase = message.toLocaleLowerCase()
+
+console.log(msgToLowerCase);
+
+
+const mySchool = 'Boca Code'
+
+//
+//
+
